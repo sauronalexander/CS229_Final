1c0cfa51a8921cd3fee6862f4b9f8cb869a0f890
JulianYG <yg1811@hotmail.com>
2017-11-18 17:27:47 -0800

    added color histogram

---
+++ Color_Histogram.py
@@ -0,0 +1,22 @@
+import numpy as np
+import cv2
+
+box_dim = 8
+box_size = box_dim*box_dim*box_dim
+
+hist = np.zeros(box_size)
+
+for i in range(0, 500):
+    image = cv2.imread('./imgs/'+str(i+1)+'.jpg')
+    for x in range(0, 511):
+        for y in range(0, 511):
+            r = image[x, y][0]
+            g = image[x, y][1]
+            b = image[x, y][2]
+            r = int(r/(256/box_dim))
+            g = int(g/(256/box_dim))
+            b = int(b/(256/box_dim))
+            index = r+g*box_dim+b*box_dim*box_dim
+            hist[index] += 1
+
+numpy.savetxt("./data/histogram.csv", a, delimiter=",")
